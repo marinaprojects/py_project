7fee22b0d2de386b3bb4873e4a1118299088b3a4
Marina Safina <safina531@gmail.com>
2024-01-14 02:57:40 -0600

    to the main.py added subtract_numbers funcs.py modified with subtract_numbers

--- lesson_code/py_project/funcs.py
+++ lesson_code/py_project/funcs.py
@@ -2,5 +2,9 @@ def add_num(a, b):
     return a + b
 
 
+def subtract_numbers(a, b):
+    return a - b
+
+
 
 
--- lesson_code/py_project/main.py
+++ lesson_code/py_project/main.py
@@ -1,4 +1,4 @@
-from funcs import add_num
+from funcs import add_num, subtract_numbers
 
 
 def main():
@@ -6,6 +6,18 @@ def main():
     b = 7
     result = add_num(a, b)
     print(f"Summ of numbers is: {result}")
+
+
+if __name__ == "__main__":
+    main()
+
+
+def main():
+    a = 1
+    b = 7
+    result = subtract_numbers(a, b)
+    print(f"subtract of numbers is: {result}")
+
 
 if __name__ == "__main__":
     main()
